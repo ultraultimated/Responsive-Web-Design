f6e766d7dba70b7b2b72e40d04914757b46c62bb
poojanp <poojanp@cybage.com>
2018-10-05 19:15:07 +0530

    Adds Home Page

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('#news .news-container img, #news .news-container h3').mouseover(function(){
+    var parent = $(this).parent();
+    parent.children('img').addClass('zoom');
+    parent.children('h3').addClass('textColor');
+});
+
+$('#news .news-container img, #news .news-container h3').mouseout(function(){
+    var parent = $(this).parent();
+    parent.children('img').removeClass('zoom');
+    parent.children('h3').removeClass('textColor');
+});
